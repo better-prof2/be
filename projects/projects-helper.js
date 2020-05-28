ead4e40ed869c6a98d4f24b332f4c9d25b7d67b2
Tatyana Dzhura <tatyanadzhura@gmail.com>
2020-05-28 18:36:21 -0400

    projects updated

--- projects/projects-helper.js
+++ projects/projects-helper.js
@@ -1,58 +1,58 @@
-const router = require("express").Router();
+const router = require("express").Router()
 
-const Projects = require("./projects-model");
-const { validateProjectId } = require("./projects-helper");
+const Projects = require("./projects-model")
+const { validateProjectId } = require("./projects-helper")
 
 router.get("/", (req, res) => {
   Projects.find()
     .then(projects => {
-      res.status(200).json(projects);
+      res.status(200).json(projects)
     })
-    .catch(err => res.send(err));
+    .catch(err => res.send(err))
 });
 
 router.get("/:id", (req, res) => {
   Projects.find();
-  const id = req.params.id;
+  const id = req.params.id
   if (!id) {
     res
       .status(404)
-      .json({ message: "The Project with the specified id does not exist." });
+      .json({ message: "The Project with the specified id does not exist." })
   } else {
     Projects.findById(id)
       .then(projects => {
-        res.status(201).json(projects);
+        res.status(201).json(projects)
       })
       .catch(err => {
         res
           .status(500)
-          .json({ message: "The project information could not be retrieved." });
+          .json({ message: "The project information could not be retrieved." })
       });
   }
 });
 
 
 router.post("/", (req, res) => {
-  const body = { ...req.body };
+  const body = { ...req.body }
   Projects.add(body)
     .then(projects => {
-      res.status(201).json(projects);
+      res.status(201).json(projects)
     })
     .catch(err => {
       console.log(err);
-      res.status(500).json({ errorMessage: "Error adding project" });
+      res.status(500).json({ errorMessage: "Error adding project" })
     });
 });
 
 router.put("/:id", validateProjectId, (req, res) => {
-  const body = { ...req.body };
+  const body = { ...req.body }
   const { id } = req.params;
   Projects.update(id, body)
     .then(updated => {
-      res.status(201).json(updated);
+      res.status(201).json(updated)
     })
     .catch(err => {
-      res.status(500).json({ errorMessage: "Could not update project." });
+      res.status(500).json({ errorMessage: "Could not update project." })
     });
 });
 
@@ -63,11 +63,11 @@ router.delete("/:id", validateProjectId, (req, res) => {
     .then(project => {
       res
         .status(200)
-        .json({ message: `Project at id ${id} was deleted.` });
+        .json({ message: `Project at id ${id} was deleted.` })
     })
     .catch(err => {
       console.log(err);
-      res.status(500).json({ error: "Could not delete Project" });
+      res.status(500).json({ error: "Could not delete Project" })
     });
 });
 
