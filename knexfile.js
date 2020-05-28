0f66034310ba98271ff2aa9ca1e912875fd89aa8
Tatyana Dzhura <tatyanadzhura@gmail.com>
2020-05-28 18:58:22 -0400

    testing and production added

--- knexfile.js
+++ knexfile.js
@@ -21,6 +21,40 @@ module.exports = {
             conn.run("PRAGMA foreign_keys = ON", done)
         }
     }
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+        filename: "./data/better_prof.db3"
+    },
+    migrations: {
+        tableName: "knex_migrations"
+    },
+    seeds: {
+        directory: "./data/seeds"
+    },
+    useNullAsDefault: true,
+    pool: {
+        afterCreate: (conn, done) => {
+            conn.run("PRAGMA foreign_keys = ON", done) 
+        }
+    }
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+        directory: "./data/migrations"
+    },
+    seeds: {
+        directory: "./data/seeds"
+    },
+    pool: {
+        min: 2,
+        max: 10
+    }
   }
 
 
